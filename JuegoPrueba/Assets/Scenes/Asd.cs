c1c782b38e15ec947a7cf71bfa3db278085320ea
Taiel2002 <42707835+Taiel2002@users.noreply.github.com>
2018-08-26 04:05:08 -0300

    ¡Hola Mundo!

---
+++ JuegoPrueba/Assets/Scenes/Asd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asd : MonoBehaviour {
+
+    int holamundo = 100;
+
+	// Use this for initialization
+	void Start () {
+        transform.Translate(0, 2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
